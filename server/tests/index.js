148fd9e325fab907abf41f1191845d0d162a394f
Emyboy <emyboybeats@gmail.com>
2018-12-26 12:04:29 +0100

    pushing

--- server/tests/index.js
+++ server/tests/index.js
@@ -1,2 +1,25 @@
-import chai from 'chai';
+const chai = require('chai');
+const chaiHTTP = require('chai-http');
+chai.use(chaiHTTP);
+import app from '../src';
+
+import recipeController from '../src/controllers/recipeController';
 const should = chai.should();
+
+
+describe('testing recipe features',()=>{
+	it('should add new recipe to the database',(done)=>{
+		const newRecipe = {
+			ingredient : 'milk',
+			userid : 1,
+			imageurl : 'www.google.com',
+			about : 'never eat this food'
+		};
+		chai.request(app).post('/api/v1/recipe')
+			.send(newRecipe).end((error,response)=>{
+				response.should.have.status(200);
+				response.body.should.be.a('object');
+			});
+		done();
+	});
+});
