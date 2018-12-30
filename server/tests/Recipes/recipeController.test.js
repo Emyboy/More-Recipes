b7b1d026067988a87c91708059c897066142c7df
Emyboy <emyboybeats@gmail.com>
2018-12-31 00:53:12 +0100

    removed the delete test

--- server/tests/Recipes/recipeController.test.js
+++ server/tests/Recipes/recipeController.test.js
@@ -27,17 +27,17 @@ describe('testing recipe controllers',()=>{
 
 	// REMEMBER TO RUN UPDATE RECIPE TEST
 
-	it('should delete a recipe by recipe id',(done)=>{
-		const id = 6;
-		chai.request(app)
-			.del(`recipes/${id}`)
-			.end((error ,res)=>{
-				res.should.have.status(200);
-				res.body.should.have.property('message').eql(`Recipe ${id} was deleted`);
-				res.body.should.be.a('object');
-			});
-		done();
-	});
+	// it('should delete a recipe by recipe id',(done)=>{
+	// 	const id = 6;
+	// 	chai.request(app)
+	// 		.del(`recipes/${id}`)
+	// 		.end((error ,res)=>{
+	// 			res.should.have.status(200);
+	// 			res.body.should.have.property('message').eql(`Recipe ${id} was deleted`);
+	// 			res.body.should.be.a('object');
+	// 		});
+	// 	done();
+	// });
 
 	it('should get all recipes from the database',(done)=>{
 		chai.request(app).get('/api/recipes')
